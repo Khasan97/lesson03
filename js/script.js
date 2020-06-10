0625a394fc519e6c1fc43a795ae8cb2e7c03056b
Khasan Mukhammetrahimov <damskiy.ugodnik1997@gmail.com>
2020-06-10 21:44:30 +0600

    third commit

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let money = +prompt('Ваш доход?', 700),
 		amount1 = +prompt('Во сколько это обойдется?'),
 		amount2 = +prompt('Во сколько это обойдется?'),
 		period = 5,//число от 1 до 12(месяцев)
-		budgetMonth = amount1 + amount2,
+		budgetMonth = money - (amount1 + amount2),
 		budgetDay = budgetMonth / 30;
 
 
@@ -21,6 +21,8 @@ let money = +prompt('Ваш доход?', 700),
 console.log(typeof money);
 console.log(typeof income);
 console.log(addExpenses.length);
+console.log(changeExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
 console.log(typeof deposit);
 console.log(period + " месяцев");
 console.log("Бюджет " + budgetMonth + " за месяц");
@@ -31,10 +33,10 @@ console.log('Цель за день ' + Math.floor(budgetDay) + ' $');
 
 if (budgetDay >= 1200) {
 	console.log("У вас высокий уровень дохода");
-} else if (budgetDay > 600 && budgetDay < 1200) {
+} else if (budgetDay > 600, budgetDay < 1200) {
 	console.log('У вас средний уровень дохода');
-} else if (budgetDay) {
+} else if (budgetDay <=600) {
 	console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay === 0 || budgetDay < 0) {
+} else if (budgetDay  < 0) {
 	console.log('Что то пошло не так');
 }
